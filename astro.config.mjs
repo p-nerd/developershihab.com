47e5407f609536c3f3194e4554df792617de77de
Shihab Mahamud <shihab4t@gmail.com>
2023-07-20 01:16:45 +0600

    Astro deploy action added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://p-nerd.github.io',
   integrations: [react(), tailwind()]
 });
