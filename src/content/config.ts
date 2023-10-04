c922e97bde0873b21bbae04a6de1ebaff480aa17
Shihab Mahamud <shihab4t@gmail.com>
2023-10-05 05:51:50 +0600

    projects page done

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,25 @@
 import { defineCollection, z } from "astro:content";
 
-const projectCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        publishDate: z.coerce.date(),
-        tags: z.array(z.string()),
-        img: z.string(),
-        img_alt: z.string().optional(),
-    }),
-});
-
-const blogCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        publishDate: z.coerce.date(),
-        tags: z.array(z.string()),
-        img: z.string(),
-        img_alt: z.string().optional(),
-    }),
-});
-
 export const collections = {
-    projects: projectCollection,
-    blogs: blogCollection,
+    projects: defineCollection({
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            publishDate: z.coerce.date(),
+            type: z.string(),
+            technologies: z.array(z.string()),
+            img: z.string(),
+            img_alt: z.string().optional(),
+        }),
+    }),
+    blogs: defineCollection({
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            publishDate: z.coerce.date(),
+            tags: z.array(z.string()),
+            img: z.string(),
+            img_alt: z.string().optional(),
+        }),
+    }),
 };
--- src/content/projects/markdown-mystery-tour.md
+++ src/content/projects/markdown-mystery-tour.md
@@ -5,10 +5,11 @@ img: /assets/stock-1.jpg
 img_alt: Iridescent ripples of a bright blue and pink liquid
 description: |
     We designed a whodunnit-style game to introduce Markdown formatting. Suspense — suspicion — syntax!
-tags:
+technologies:
     - Design
     - Dev
     - User Testing
+type: ERP
 ---
 
 ## Level-two heading
