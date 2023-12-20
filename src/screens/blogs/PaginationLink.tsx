88aff327210bcc25b77efc1aecc46b6014722057
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2023-12-21 00:45:56 +0600

    convert to solid component

---
+++ src/screens/blogs/PaginationLink.tsx
@@ -0,0 +1,15 @@
+import { mergeProps } from "solid-js";
+
+const PaginationLink = (props: { link?: string; text?: string }) => {
+    const p = mergeProps({ link: "/", text: "More blogs" }, props);
+    return (
+        <a
+            href={p.link}
+            class="flex cursor-pointer flex-col items-center rounded-xl border bg-[#F9F9FB] py-2 text-[#7611a6] dark:border-[#283043] dark:bg-[#0F1119]"
+        >
+            <p>{p.text}</p>
+        </a>
+    );
+};
+
+export default PaginationLink
