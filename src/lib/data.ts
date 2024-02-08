90e0666f9526b4fa86b24c3b8bfa8d41e39dcba6
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-02-08 16:21:59 +0600

    move files to src folder

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import corpcommentImg from "@/assets/projects/corpcomment.png";
+import rmtdevImg from "@/assets/projects/rmtdev.png";
+import wordanalyticsImg from "@/assets/projects/wordanalytics.png";
 
 export const links = [
     {
