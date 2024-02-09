2cdbcff68bce10dc89f8528bde1e926ac9c7c032
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-02-10 05:02:06 +0600

    blog page

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,7 +3,7 @@ import person from "@/conf/person";
 const Footer = () => {
     const year = new Date().getFullYear();
     return (
-        <footer className="mb-10 px-4 text-center text-gray-500">
+        <footer className="mb-10 mt-10 px-4 text-center text-gray-500">
             <small className="mb-2 block text-xs">
                 &copy; {year} {person.name}. All rights reserved.
             </small>
