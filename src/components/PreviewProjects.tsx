c78e57ea97e7f49d94215c2ab48564f9809c17fb
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-02-14 15:25:37 +0600

    fix projects page links descriptive text

--- src/components/PreviewProjects.tsx
+++ src/components/PreviewProjects.tsx
@@ -30,11 +30,11 @@ const Project = (project: TProject) => {
             <section className="relative  overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[25rem] sm:pr-8 sm:group-even:pl-8">
                 <div className="flex h-full flex-col justify-between px-5 pb-7 pt-4 sm:max-w-[55%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
                     <div className="flex flex-col gap-2">
-                        <h3 className="text-2xl font-semibold">{project.title}</h3>
+                        <h2 className="text-2xl font-semibold">{project.title}</h2>
                         <p className="leading-relaxed text-gray-700 dark:text-white/70">
                             {project.description}{" "}
                             {!!project.slug && (
-                                <Link href={`/projects/${project.slug}`} className="underline">
+                                <Link href={`/projects/${project.slug}`} className="underline" aria-label={project.title} >
                                     Read more
                                 </Link>
                             )}
