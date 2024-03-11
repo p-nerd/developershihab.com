98f2f527538aa619c7c3e00c019dafb45ceb0b0c
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-03-11 13:54:45 +0600

    linked in

--- src/conf/person.ts
+++ src/conf/person.ts
@@ -2,7 +2,7 @@ const person = {
     name: "Shihab Mahamud",
     facebook: "https://facebook.com/shihab4t",
     twitter: "https://twitter.com/@p_nerdx",
-    linkedin: "https://linkedin.com/in/p-nerd",
+    linkedin: "https://linkedin.com/in/shihab-mahamud",
     github: "https://github.com/p-nerd",
     dev_to: "https://dev.to/p-nerd",
     email: "shihab4t@gmail.com",
